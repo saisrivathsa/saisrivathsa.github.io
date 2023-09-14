6a6571f06eb54474ca88808dbf62ad6bb6567eb7
saisrivathsa <srivathsaeric@gmail.com>
2023-09-14 11:19:34 +0530

    Change url

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "0c275d88c947e1ff9d5949bd1e7bd411",
-"index.html": "0b4597269a1841f29fdb471efb6545d6",
-"/": "0b4597269a1841f29fdb471efb6545d6",
-"main.dart.js": "50e9ae172a5ad18c238e145fe511d86e",
+"index.html": "b4c9d7f1a6c0857b98d4b33d4d2ba387",
+"/": "b4c9d7f1a6c0857b98d4b33d4d2ba387",
+"main.dart.js": "c920b3049baa3a3a7d73d858b1cdfd97",
 "flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
